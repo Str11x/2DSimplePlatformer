a84160d0afa91909d33d31c54d0a181c2851d126
Str11x <101504581+Str11x@users.noreply.github.com>
2022-07-12 23:12:24 +0500

    Correct movement and DoubleJump

--- Platformer/Assets/Scripts/PlayerController.cs
+++ Platformer/Assets/Scripts/PlayerController.cs
@@ -5,14 +5,17 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     [SerializeField] private float _speed = 10;
-    [SerializeField] private float _jumpForce;
+    [SerializeField] private float _jumpForce = 15;
+    [SerializeField] private float maximumNearDistance = 0.99f;
 
     private Rigidbody2D _rigidbody;
     private SpriteRenderer _spriteRenderer;
 
     private float _movement;
     private bool _isJump;
-    private bool _isGrounded;
+    private bool _isGrounded = true;
+    private bool _isCanDoubleJump;
+    private bool _readyToNextJump = true;
 
     //private RaycastHit2D hit2D;
 
@@ -26,7 +29,7 @@ public class PlayerController : MonoBehaviour
     {
         _movement = Input.GetAxis("Horizontal");
 
-        if (Input.GetKeyDown(KeyCode.Space) && _isGrounded)
+        if (Input.GetKeyDown(KeyCode.Space) && _readyToNextJump)
            _isJump = true;
 
         if (_movement < 0)
@@ -39,7 +42,9 @@ public class PlayerController : MonoBehaviour
     {
         Move();
         FindOutGround();
-        Jump();
+
+        if (_isJump)
+            Jump();
     }
 
     private void Move()
@@ -49,21 +54,35 @@ public class PlayerController : MonoBehaviour
 
     private void Jump()
     {
-        if (_isJump)
+        if (_isGrounded)
+        {
+            _isCanDoubleJump = true;
             _rigidbody.velocity = Vector3.up * _jumpForce;
-
-        _isJump = false;
+            _isJump = false;
+        }
+        else if(_isCanDoubleJump)
+        {
+            _isCanDoubleJump = false;
+            _rigidbody.velocity = Vector3.up * _jumpForce;
+            _isJump = false;
+            _readyToNextJump = false;
+        }
     }
 
     private void FindOutGround()
     {
         RaycastHit2D hit2D = Physics2D.Raycast(transform.position, -Vector2.up);
 
-        float distance = Vector3.Distance(transform.position, hit2D.point);
+        float distance = Vector3.Distance(transform.position, hit2D.point);  
 
-        if (hit2D == true && distance < 5 )
-            _isGrounded = true; 
+        if (hit2D == true && distance < maximumNearDistance)
+        {
+            _isGrounded = true;
+            _readyToNextJump = true;
+        }
         else
+        {
             _isGrounded = false;
+        }         
     }
 }
