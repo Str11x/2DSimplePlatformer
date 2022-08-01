7ccbb963ec0fba5cb72c8e9988339593bc78e3fa
Str11x <101504581+Str11x@users.noreply.github.com>
2022-08-01 11:34:08 +0500

    Renaming

--- Platformer/Assets/Scripts/EffectsLibrary.cs
+++ Platformer/Assets/Scripts/EffectsLibrary.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class EffectsManager : MonoBehaviour
+public class EffectsLibrary : MonoBehaviour
 {
     [SerializeField] private PlayerController _player;
     [SerializeField] private Score _scoreCount;
